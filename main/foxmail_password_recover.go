153551e1f610f1f90a6e805006500a89f0a2ff6e
ryoii <ryoii@foxmail.com>
2023-01-06 19:21:23 +0800

    Support load from arguments

--- main/foxmail_password_recover.go
+++ main/foxmail_password_recover.go
@@ -10,34 +10,71 @@ import (
 )
 
 func main() {
-	loadAll()
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println("Press Enter to exit...")
+		_, _ = fmt.Scanln()
+	}()
 
-	fmt.Println("Press Enter to exit...")
-	_, _ = fmt.Scanln()
+	args := os.Args
+	if len(args) > 1 {
+		loadFromPath(args[1])
+	} else {
+		loadFromRegistry()
+	}
 }
 
-func loadAll() {
+// loadFromPath read the password from the path given with arguments.
+// If the path is a file, it will be loaded as rec0 file.
+// If the path is a directory, it will be solved as the Storage directory.
+func loadFromPath(path string) {
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	stat, err := file.Stat()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	if !stat.IsDir() {
+		password := loadPasswordFromFile(path)
+		fmt.Println(password)
+		return
+	} else {
+		solveStorage(path)
+	}
+}
+
+// Get the Storage directory from registry
+func loadFromRegistry() {
 	location := registry.GetStorageLocation()
 	dir := filepath.Dir(location)
+	solveStorage(filepath.Join(dir, "Storage"))
+}
 
-	base := filepath.Join(dir, "Storage")
-	accounts, _ := os.ReadDir(base)
+func solveStorage(storagePath string) {
+	accounts, _ := os.ReadDir(storagePath)
 	for _, account := range accounts {
 		info, err := account.Info()
 		if err != nil {
 			continue
 		}
-		solveFromAccountDir(base, info)
+		solveAccountDir(storagePath, info)
 	}
 }
 
-func solveFromAccountDir(base string, account os.FileInfo) {
+func solveAccountDir(base string, account os.FileInfo) {
 	fileName := filepath.Join(base, account.Name(), "Accounts", "Account.rec0")
-	password := loadAllSingleFile(fileName)
+	password := loadPasswordFromFile(fileName)
 	fmt.Printf("%-40s%s\n", account.Name(), password)
 }
 
-func loadAllSingleFile(fileName string) string {
+func loadPasswordFromFile(fileName string) string {
 	context := io.ReadFile(fileName)
 
 	clientType := io.GetClientType(context)
